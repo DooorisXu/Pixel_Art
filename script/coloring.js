89a236287d07078b6922a9920ac75e5e9d785284
DooorisXu <lingyi19329@gmail.com>
2017-12-30 15:21:18 -0500

    fixed the syntax bug in the definition of the functions; need to fix the bug--function not called

--- script/coloring.js
+++ script/coloring.js
@@ -1,11 +1,13 @@
+//make the color change when the user clicks
+//need to fix the bug
 var penColor = 'black';
 
-function setPenColor(pen)
-    {
-        penColor = pen;
-    }
+function setPenColor(pen) {
+    "use strict";
+    penColor = pen;
+}
 
-function setPixelColor(pixel)
-    {
-        pixel.style.backgroundColor = penColor;
-    }
+function setPixelColor(pixel) {
+    "use strict";
+    pixel.style.backgroundColor = penColor;
+}
