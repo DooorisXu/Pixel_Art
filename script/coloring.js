1023bb54316797ead08da6eb15109d42c976bc50
DooorisXu <lingyi19329@gmail.com>
2017-12-30 14:36:06 -0500

    edited the statics

--- script/coloring.js
+++ script/coloring.js
@@ -1,4 +1,11 @@
-function setPixelColour(pixel)
-{
-    pixel.style.backgroundColor = 'black';
-}
+var penColor = 'black';
+
+function setPenColor(pen)
+    {
+        penColor = pen;
+    }
+
+function setPixelColor(pixel)
+    {
+        pixel.style.backgroundColor = penColor;
+    }
